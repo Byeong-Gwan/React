59f6ae22f7515fbad3a1c791a0a866a03a362b05
김병관 <103481904+Byeong-Gwan@users.noreply.github.com>
2023-04-25 15:52:27 +0900

    props 공부

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,13 +1,25 @@
 // import './App.css';
+import Container from './Container';
 import Counter from './Counter';
 import MyHeader from './MyHeader.js';
 
 function App() {
+
+    const counterProps = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 5,
+    };
+
     return (
-        <div>
-            <MyHeader />
-            <Counter />
-        </div>
+        <Container>
+            <div>
+                <MyHeader />
+                <Counter {...counterProps} />
+            </div>
+        </Container>
     );
 }
 
