93f222f811db13ab3294d2b605a9b16fc24f805a
김병관 <103481904+Byeong-Gwan@users.noreply.github.com>
2023-04-25 14:00:49 +0900

    React 공부

---
+++ reactexam1/src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import Counter from './Counter';
+import MyHeader from './MyHeader.js';
+
+function App() {
+    return (
+        <div>
+            <MyHeader />
+            <Counter />
+        </div>
+    );
+}
+
+export default App;
